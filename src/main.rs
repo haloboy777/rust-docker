5e944e77a3dc8d74195d586f29bbc6ebf971fb0c
Ayush Singh <ayush@intervue.io>
2023-06-21 01:43:32 +0530

    AS - env shit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use actix_web::middleware::Logger;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
+// use std::collections::HashMap;
+// use std::env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -13,11 +15,18 @@ async fn healthcheck() -> impl Responder {
     HttpResponse::Ok()
 }
 
-
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
+
+// #[get("/getenv")]
+// async fn getenv(_: String) -> impl Responder {
+//     let envvals: HashMap<_, _> = env::vars().collect();
+//     let serialized_data = serde_json::to_string(&envvals).unwrap();
+
+//     HttpResponse::Ok().body(serialized_data)
+// }
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
@@ -26,6 +35,7 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     SimpleLogger::new()
         .with_level(LevelFilter::Info)
         .init()
@@ -36,6 +46,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(healthcheck)
             .service(echo)
+            // .service(getenv)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
