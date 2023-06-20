c5d85b9e189244b1096b7c3d4190bcfd4d44eaf8
Ayush Singh <ayush@intervue.io>
2023-06-20 16:33:34 +0530

    AS - added release strategy

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/healthcheck")]
+async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
@@ -28,6 +34,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .service(hello)
+            .service(healthcheck)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
