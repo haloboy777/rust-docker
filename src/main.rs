e9bf46b5ba6c0bdef47bdea22ee15e0c86635034
Ayush Singh <ayush@intervue.io>
2023-06-20 15:19:03 +0530

    AS - might use turso

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use actix_web::middleware::Logger;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -16,8 +19,14 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // env_logger::init_from_env(Env::default().default_filter_or("info"));
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
